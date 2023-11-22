c4ab39586d73873f998c7e7d64aaccb56a78d4d5
James Hassett <37683814+jameshas@users.noreply.github.com>
2023-11-23 00:09:42 +1100

    Update generate_doh_lists.py

--- generate_doh_lists.py
+++ generate_doh_lists.py
@@ -1,13 +1,17 @@
-import time
-from functools import wraps
+import os
 import urllib.request
+import urllib.error
 from urllib.parse import urlparse
-import re
 import socket
+import re
 import json
+import time
 from datetime import datetime
+import textwrap
+from functools import wraps
 
 
+_IPIFY_ADDRESS = 'https://geo.ipify.org/api/v2/country?apiKey=' + os.getenv('APIKEY_IPIFY') # type: ignore
 _CURL_DOH_WIKI_ADDRESS = 'https://raw.githubusercontent.com/wiki/curl/curl/DNS-over-HTTPS.md'
 _ADGUARD_DOH_WIKI_ADDRESS = 'https://raw.githubusercontent.com/AdguardTeam/KnowledgeBaseDNS/master/docs/general/dns-providers.md'
 
@@ -27,6 +31,19 @@ def timer(func):  # Decorator
     return wrapper
 
 
+def get_ip_info_string():
+    '''Get geolocation based on IP and return "Region, Country" string.'''
+    try:
+        with urllib.request.urlopen(_IPIFY_ADDRESS) as response:
+            ip_info = json.loads(response.read().decode('utf-8'))
+            return f"{ip_info['location']['region']}, {ip_info['location']['country']}"
+    except urllib.error.HTTPError as e:
+        if e.code == 403: # Capture 403 forbidden, throw otherwise.
+            return f"Error 403 (forbidden), check ipify API key"
+        else:
+            raise
+
+
 def strip_port(domain):
     '''Strips a port number if present in a domain (e.g. example.com:853 > example.com).'''
     if ":" in domain:
@@ -38,6 +55,32 @@ def strip_port(domain):
 def get_valid_domain(url):
     '''Gets a plain domain by parsing a URL and striping port numbers if present.'''
     return strip_port(urlparse(url).netloc)
+
+
+def sort_domains(domains):
+    """Sorts a list of domains in alphabetical order by SLD, then subdomains,
+    ignoring TLD.
+
+    Parameters
+    ----------
+    domains : list
+        The list of domains to sort
+
+    Returns
+    -------
+    list
+        The list of sorted domains
+    """
+    # Split each domain into it's parts (TLD, SLD, subdomains).
+    split_domains = [domain.split('.')[::-1] for domain in domains]
+
+    # Sort by SLD, then by each subdomain, ignoring TLD.
+    sorted_domains = sorted(split_domains, key=lambda x: x[1:])
+
+    # Join the domain parts back together, reverse them to original order.
+    sorted_domains = ['.'.join(domain[::-1]) for domain in sorted_domains]
+
+    return sorted_domains
 
 
 def get_file_doh_domains(filename, domains=[]):
@@ -54,23 +97,24 @@ def get_file_doh_domains(filename, domains=[]):
 
     Returns
     -------
-    list
-        Parsed domains
+    domains : list
+        Parsed, non-duplicate domains
+    stats : dict
+        Match statistics
     """
-    matches = 0
-    adds = 0
+    stats = {'matches': 0, 'adds': 0}
 
     with open(filename, 'r') as file:
         for line in file:
             domain = line.strip()
             if domain:
-                matches += 1
+                stats['matches'] += 1
                 if not domain in domains:
-                    adds += 1
+                    stats['adds'] += 1
                     domains.append(domain)
                 
-    print(f"Found {matches} domains in file '{filename}'. Added {adds} non-duplicate domain/s to the list.")
-    return domains
+    print(f"Found {stats['matches']} domains in file '{filename}'. Added {stats['adds']} non-duplicate domain/s to the list.")
+    return domains, stats
 
 
 def get_curl_wiki_doh_domains(domains=[]):
@@ -83,8 +127,10 @@ def get_curl_wiki_doh_domains(domains=[]):
 
     Returns
     -------
-    list
-        Found valid domains
+    domains : list
+        Found valid, non-duplicate domains
+    stats : dict
+        Match statistics
     """
     # Retrieve the raw wiki content
     with urllib.request.urlopen(_CURL_DOH_WIKI_ADDRESS) as response:
@@ -96,8 +142,7 @@ def get_curl_wiki_doh_domains(domains=[]):
     trimmed_wiki_content = wiki_content[start:end]
 
     # Find all of the domains listed in the table.
-    matches = 0
-    adds = 0
+    stats = {'matches': 0, 'adds': 0}
     rows = trimmed_wiki_content.split('\n')
     for row in rows:
         # Split the row into columns based on MD table formatting ('|')
@@ -116,14 +161,14 @@ def get_curl_wiki_doh_domains(domains=[]):
                 domain = get_valid_domain(url_match.group())
                 # If valid domain found
                 if domain:
-                    matches += 1
+                    stats['matches'] += 1
                     # If domain is not a duplicate
                     if not domain in domains:
-                        adds += 1
+                        stats['adds'] += 1
                         domains.append(domain)
 
-    print(f"Found {matches} DoH domains in the Curl DoH Wiki. Added {adds} non-duplicate domain/s to the list.")
-    return domains
+    print(f"Found {stats['matches']} DoH domains in the Curl DoH Wiki. Added {stats['adds']} non-duplicate domain/s to the list.")
+    return domains, stats
 
 
 def get_adguard_wiki_doh_domains(domains=[]):
@@ -136,16 +181,17 @@ def get_adguard_wiki_doh_domains(domains=[]):
 
     Returns
     -------
-    list
-        Found valid domains
+    domains : list
+        Found valid, non-duplicate domains
+    stats : dict
+        Match statistics
     """
     # Retrieve the raw wiki content
     with urllib.request.urlopen(_ADGUARD_DOH_WIKI_ADDRESS) as response:
         wiki_content = response.read().decode('utf-8')
 
     # Find all of the DoH domains in the MD tables in the content
-    matches = 0
-    adds = 0
+    stats = {'matches': 0, 'adds': 0}
     rows = wiki_content.split('\n')
     for row in rows:
         # Split the row into columns based on MD table formatting ('|')
@@ -162,14 +208,14 @@ def get_adguard_wiki_doh_domains(domains=[]):
                 for url_match in url_matches:
                     domain = get_valid_domain(url_match)
                     if domain:
-                        matches += 1
+                        stats['matches'] += 1
                         # If domain is not a duplicate
                         if not domain in domains:
-                            adds += 1
+                            stats['adds'] += 1
                             domains.append(domain)
     
-    print(f"Found {matches} DoH domains in the AdGuard DNS Wiki. Added {adds} non-duplicate domain/s to the list.")
-    return domains
+    print(f"Found {stats['matches']} DoH domains in the AdGuard DNS Wiki. Added {stats['adds']} non-duplicate domain/s to the list.")
+    return domains, stats
 
 
 @timer
@@ -187,6 +233,8 @@ def resolve_domains(domains):
         The domains resolved to IPs {"example.com": [1.1.1.1, 1.0.0.1],}
     ips_flat : list
         All resolved IPs, no domain names
+    failed_domains : list
+        Domain names that failed to resolve
     """
     ips_dict = {}
     ips_flat = []
@@ -208,24 +256,27 @@ def resolve_domains(domains):
             failed_domains.append(domain)
 
     print(f"Resolved {len(domains)-len(failed_domains)}/{len(domains)} domains to IPs. Failed domains: {failed_domains if len(failed_domains) > 0 else 'none'}.")
-    return ips_dict, ips_flat
+    return ips_dict, ips_flat, failed_domains
 
 
 def main():
+    # Date, time string for the run time.
+    time_now = f"{datetime.utcnow()} UTC"
+
     # Get the static known domains from file.
-    domains = get_file_doh_domains('doh_static_known_domains.txt')
+    domains, stats_file = get_file_doh_domains('doh_static_known_domains.txt')
 
-    # Get domains from Curl Wiki (append).
-    domains = get_curl_wiki_doh_domains(domains)
+    # Get domains from Curl Wiki (append without duplicates).
+    domains, stats_curl = get_curl_wiki_doh_domains(domains)
     
-    # Get domains from AdGuard Wiki (append).
-    domains = get_adguard_wiki_doh_domains(domains)
+    # Get domains from AdGuard Wiki (append without duplicates).
+    domains, stats_adguard = get_adguard_wiki_doh_domains(domains)
 
-    # Alphabetical domain ordering for easier change tracking.
-    domains.sort()
+    # Proper domain ordering for easier change tracking.
+    domains = sort_domains(domains)
 
     # Resolve all domains to IP addresses.
-    ips_dict, ips_flat = resolve_domains(domains)
+    ips_dict, ips_flat, failed_domains = resolve_domains(domains)
     
     # Proper IP ordering for easier change tracking.
     ips = sorted(ips_flat, key=socket.inet_aton)
@@ -251,7 +302,7 @@ def main():
     # Write AdBlock-style domains to txt file.
     with open('lists/doh_domains_adblock.txt', 'w') as file:
         # Add a header
-        header = f"!\n! Title: Public DoH Domains\n! Last modified: {datetime.now()}\n! Domain count: {len(domains)}\n!\n! Generated by @jameshas/Public-DoH-Lists\n!\n"
+        header = f"!\n! Title: Public DoH Domains\n! Last updated: {time_now}\n! Domain count: {len(domains)}\n!\n! Generated by @jameshas/Public-DoH-Lists\n!\n"
         file.write(header)
 
         # Write domains to file "||domain.com^"
@@ -263,7 +314,7 @@ def main():
     # Write Hosts-style domains to txt file.
     with open('lists/doh_domains_hosts.txt', 'w') as file:
         # Add a header
-        header = f"#\n# Title: Public DoH Domains\n# Last modified: {datetime.now()}\n# Domain count: {len(domains)}\n#\n# Generated by @jameshas/Public-DoH-Lists\n#\n"
+        header = f"#\n# Title: Public DoH Domains\n# Last updated: {time_now}\n# Domain count: {len(domains)}\n#\n# Generated by @jameshas/Public-DoH-Lists\n#\n"
         file.write(header)
 
         # Write domains to file "0.0.0.0 domain.com"
@@ -271,6 +322,33 @@ def main():
             file.write(f"0.0.0.0 {domain}")
             if index != len(domains) - 1:
                 file.write('\n')
+
+    # Create a run stats string output.
+    run_stats = textwrap.dedent(f"""\
+        <!-- start_run-stats -->
+        **Last run output**
+        ```
+        Ran at: {time_now}
+        Unique domains: {len(domains)}
+          - From file: {stats_file['adds']} unique ({stats_file['matches']} found)
+          - From Curl wiki: {stats_curl['adds']} unique ({stats_curl['matches']} found)
+          - From AdGuard wiki: {stats_adguard['adds']} unique ({stats_adguard['matches']} found)
+        Unique IPs: {len(ips_flat)}
+          - {len(failed_domains)} domains failed to resolve
+          - Resolved local to {get_ip_info_string()}
+        ```
+        <!-- end_run-stats -->""")
+
+    # Read the existing README.md.
+    with open('README.md', 'r') as file:
+        content = file.read()
+
+    # Substitute the runstats section with the new output.
+    new_content = re.sub(r'<!-- start_run-stats -->(.|\s)*?<!-- end_run-stats -->', run_stats, content)
+
+    # Write the runstats updates back to README.md.
+    with open('README.md', 'w') as file:
+        file.write(new_content)
 
 
 if __name__ == '__main__':
